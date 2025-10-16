447bc24980c6b077ec630f9096cba737239097e2
Magicflyingcow <47694609+Magicflyingcow@users.noreply.github.com>
2025-10-16 22:43:25 +0100

    Align control styling with profile sensor

--- src/components/ui/slider.jsx
+++ src/components/ui/slider.jsx
@@ -1,7 +1,27 @@
 import { useId } from "react";
 import { cn } from "@/lib/utils";
 
-export function Slider({ value = [0], min = 0, max = 100, step = 1, onValueChange, className }) {
+const singleThumbStyles =
+  "h-[14px] w-[14px] rounded-full border border-gray-400 bg-white shadow-sm transition-shadow focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-1 focus-visible:ring-gray-400";
+
+const singleTrackStyles =
+  "h-2 w-full cursor-pointer appearance-none rounded-full bg-slate-200 accent-gray-700 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-1 focus-visible:ring-gray-400";
+
+const multiThumbStyles =
+  "pointer-events-auto h-2 w-full cursor-pointer appearance-none bg-transparent accent-gray-700 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-1 focus-visible:ring-gray-400";
+
+const thumbPseudoStyles = cn(
+  `[&::-webkit-slider-thumb]:${singleThumbStyles}`,
+  "[&::-moz-range-thumb]:h-[14px]",
+  "[&::-moz-range-thumb]:w-[14px]",
+  "[&::-moz-range-thumb]:border",
+  "[&::-moz-range-thumb]:border-gray-400",
+  "[&::-moz-range-thumb]:rounded-full",
+  "[&::-moz-range-thumb]:bg-white",
+  "[&::-moz-range-thumb]:shadow-sm"
+);
+
+export function Slider({ value = [0], min = 0, max = 100, step = 1, onValueChange, className, ...props }) {
   const id = useId();
   const handleSingleChange = (event) => {
     const next = Number(event.target.value);
@@ -13,15 +33,13 @@ export function Slider({ value = [0], min = 0, max = 100, step = 1, onValueChang
       <input
         id={id}
         type="range"
-        className={cn(
-          "h-2 w-full cursor-pointer appearance-none rounded-full bg-slate-200 accent-sky-500 [&::-webkit-slider-thumb]:h-4 [&::-webkit-slider-thumb]:w-4 [&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:bg-sky-500 [&::-moz-range-thumb]:h-4 [&::-moz-range-thumb]:w-4 [&::-moz-range-thumb]:rounded-full [&::-moz-range-thumb]:border-0 [&::-moz-range-thumb]:bg-sky-500",
-          className
-        )}
+        className={cn(singleTrackStyles, thumbPseudoStyles, className)}
         min={min}
         max={max}
         step={step}
         value={value[0]}
         onChange={handleSingleChange}
+        {...props}
       />
     );
   }
@@ -37,11 +55,13 @@ export function Slider({ value = [0], min = 0, max = 100, step = 1, onValueChang
     onValueChange?.(next);
   };
 
+  const { list: _list, ...rangeProps } = props;
+
   return (
     <div className={cn("relative flex w-full items-center", className)}>
       <div className="absolute left-0 right-0 h-1 rounded-full bg-slate-200" />
       <div
-        className="absolute h-1 rounded-full bg-sky-500"
+        className="absolute h-1 rounded-full bg-gray-700"
         style={{
           left: `${((value[0] - min) / (max - min)) * 100}%`,
           right: `${100 - ((value[1] - min) / (max - min)) * 100}%`,
@@ -54,7 +74,8 @@ export function Slider({ value = [0], min = 0, max = 100, step = 1, onValueChang
         step={step}
         value={value[0]}
         onChange={handleRangeChange(0)}
-        className="pointer-events-auto h-2 w-full cursor-pointer appearance-none bg-transparent accent-sky-500 [&::-webkit-slider-thumb]:h-4 [&::-webkit-slider-thumb]:w-4 [&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:bg-sky-500 [&::-moz-range-thumb]:h-4 [&::-moz-range-thumb]:w-4 [&::-moz-range-thumb]:rounded-full [&::-moz-range-thumb]:border-0 [&::-moz-range-thumb]:bg-sky-500"
+        className={cn(multiThumbStyles, thumbPseudoStyles)}
+        {...rangeProps}
       />
       <input
         type="range"
@@ -63,7 +84,8 @@ export function Slider({ value = [0], min = 0, max = 100, step = 1, onValueChang
         step={step}
         value={value[1]}
         onChange={handleRangeChange(1)}
-        className="pointer-events-auto h-2 w-full cursor-pointer appearance-none bg-transparent accent-sky-500 [&::-webkit-slider-thumb]:h-4 [&::-webkit-slider-thumb]:w-4 [&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:bg-sky-500 [&::-moz-range-thumb]:h-4 [&::-moz-range-thumb]:w-4 [&::-moz-range-thumb]:rounded-full [&::-moz-range-thumb]:border-0 [&::-moz-range-thumb]:bg-sky-500"
+        className={cn(multiThumbStyles, thumbPseudoStyles)}
+        {...rangeProps}
       />
     </div>
   );
